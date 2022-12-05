f87bd7730eeee3eb5fde94600d82f86260abda3b
foxmon <jjockrod@naver.com>
2022-12-05 16:11:27 +0900

    Feat: MyErrorBoundary

---
+++ src/components/errorBoundary/MyErrorBoundary.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { Box, Typography } from "@mui/material";
+
+const IS_DEV_MODE = true;
+
+// TODO: 추후 스타일링 필요
+const MyErrorBoundary = ({ error }) => {
+  useEffect(() => {
+    IS_DEV_MODE && console.log(error);
+  }, []);
+
+  return (
+    <Box
+      style={{
+        width: "100%",
+        height: "90vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Typography>This is Error</Typography>
+      <a href="/" style={{ color: "black" }}>
+        GO BACK
+      </a>
+    </Box>
+  );
+};
+
+export default MyErrorBoundary;
