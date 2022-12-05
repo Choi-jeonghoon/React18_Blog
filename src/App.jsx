f0e3901cf7fdbf9c33d2ddaa0938c648a4680699
foxmon <jjockrod@naver.com>
2022-12-05 16:11:33 +0900

    Feat: App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { ErrorBoundary } from "react-error-boundary";
+import MyRouter from "./MyRouter";
+import MyErrorBoundary from "./components/errorBoundary/MyErrorBoundary";
+
 function App() {
-  return <div>asdfsafsafdsaf</div>;
+  return (
+    <ErrorBoundary fallback={<MyErrorBoundary />}>
+      <MyRouter />
+    </ErrorBoundary>
+  );
 }
 
 export default App;
