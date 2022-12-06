5380d65e166ca3f08acb3fdf3a4fae45895e83f4
foxmon <jjockrod@naver.com>
2022-12-06 13:34:14 +0900

    Fix: Header style

--- src/components/commonLayout/Header.jsx
+++ src/components/commonLayout/Header.jsx
@@ -3,10 +3,10 @@ import MenuIcon from "@mui/icons-material/Menu";
 
 const Header = () => {
   return (
-    <AppBar position="relatvie">
+    <AppBar position="relative">
       <Toolbar>
         <MenuIcon sx={{ mr: 2 }} />
-        <Typography variant="h6" color="inherit" nowrap>
+        <Typography variant="h6" color="inherit" noWrap={true}>
           MINI BLOG
         </Typography>
       </Toolbar>
