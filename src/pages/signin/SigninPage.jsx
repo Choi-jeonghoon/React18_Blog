becce46493d604f8c0264dbbd1fd8d4b06de3a69
foxmon <jjockrod@naver.com>
2022-12-06 13:28:31 +0900

    Feat: SigninPage

--- src/pages/signin/SigninPage.jsx
+++ src/pages/signin/SigninPage.jsx
@@ -1,7 +1,15 @@
+import useUserLogin from "../../hooks/login/useUserLogin";
 import SigninContainer from "../../components/container/SigninContainer";
 
 const SigninPage = () => {
-  return <SigninContainer />;
+  const { register, handleSubmit, handleUserLoginRequest } = useUserLogin();
+
+  return (
+    <SigninContainer
+      register={register}
+      onUserLoginRequestEvent={handleSubmit(handleUserLoginRequest)}
+    />
+  );
 };
 
 export default SigninPage;
