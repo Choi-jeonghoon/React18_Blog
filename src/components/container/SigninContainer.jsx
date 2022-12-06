0583762a00c8cf8e40e6c0f2e517618ac6d950b1
foxmon <jjockrod@naver.com>
2022-12-06 11:30:03 +0900

    Feat: SigninContainer

---
+++ src/components/container/SigninContainer.jsx
@@ -0,0 +1,33 @@
+import { Paper, Box, Grid } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import SigninSide from "../signin/SiginSide";
+import SigninForm from "../signin/SigninForm";
+import SigninHeader from "../signin/SigninHeader";
+
+const theme = createTheme();
+
+const SigninContainer = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <SigninSide />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <SigninHeader />
+            <SigninForm />
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+};
+
+export default SigninContainer;
